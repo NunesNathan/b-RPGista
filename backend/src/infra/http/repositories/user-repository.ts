b49561c6078bc30a3531832ff527c621c75d72fe
Nathan Nunes <nunesnathan@pop-os.localdomain>
2022-12-28 16:42:24 -0300

    feat: create find unique user repository

--- backend/src/infra/http/repositories/user-repository.ts
+++ backend/src/infra/http/repositories/user-repository.ts
@@ -3,5 +3,7 @@ import { User } from "@application/entities/user";
 export abstract class UserRepository {
   abstract findMany(): Promise<User[]>;
 
+  abstract find(id: string): Promise<User>;
+
   abstract create(user: User): Promise<User>;
 }
