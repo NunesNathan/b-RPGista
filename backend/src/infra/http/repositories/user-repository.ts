67f065c6a2d120b6465213fcf11d6fdde7868d7a
Nathan Nunes <nunesnathan@pop-os.localdomain>
2022-12-29 10:25:45 -0300

    feat: get user favorite list on repository

--- backend/src/infra/http/repositories/user-repository.ts
+++ backend/src/infra/http/repositories/user-repository.ts
@@ -1,9 +1,12 @@
+import { Favorites } from "@application/entities/favorites";
 import { User } from "@application/entities/user";
 
 export abstract class UserRepository {
   abstract findMany(): Promise<User[]>;
 
   abstract find(id: string): Promise<User>;
+
+  abstract findFavorites(id: string): Promise<Favorites>;
 
   abstract create(user: User): Promise<User>;
 
