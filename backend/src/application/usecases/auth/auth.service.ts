8a46f297cc91ec5133d87b573cf15928d0e0e0a0
Nathan Nunes <nunesnathan@pop-os.localdomain>
2022-12-30 15:21:12 -0300

    feat: login jwt

--- backend/src/application/usecases/auth/auth.service.ts
+++ backend/src/application/usecases/auth/auth.service.ts
@@ -1,13 +1,30 @@
 import { Injectable } from "@nestjs/common";
 import * as bcrypt from "bcrypt";
 import { UserRepository } from "@infra/http/repositories/user-repository";
-import { UserViewModel } from "@infra/http/viewmodels/user-view-model";
+import {
+  HttpUser,
+  UserViewModel,
+} from "@infra/http/viewmodels/user-view-model";
+import { JwtService } from "@nestjs/jwt";
+import { JwtToken, UserPayload } from "./models/auth-types";
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userRepository: UserRepository) {}
-  login(username: string, password: string) {
-    throw new Error("Method not implemented.");
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  login(validUser: HttpUser): JwtToken {
+    const payload: UserPayload = {
+      sub: validUser.id,
+      email: validUser.email,
+      username: validUser.username,
+    };
+
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 
   async validateUser(email: string, password: string) {
