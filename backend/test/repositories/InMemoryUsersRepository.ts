595bac7e626726036018da87343ead9947c3d485
Nathan Nunes <nunesnathan@pop-os.localdomain>
2022-12-30 12:27:58 -0300

    feat: in memory repository find by email

--- backend/test/repositories/InMemoryUsersRepository.ts
+++ backend/test/repositories/InMemoryUsersRepository.ts
@@ -5,6 +5,33 @@ import { UserRepository } from "@infra/http/repositories/user-repository";
 export class InMemoryUserRepository implements UserRepository {
   public users: User[] = [];
 
+  async findMany(): Promise<User[]> {
+    return this.users;
+  }
+
+  async find(id: string): Promise<User> {
+    return this.users.filter((user) => user.id === id)[0];
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    return this.users.filter((user) => user.email === email)[0];
+  }
+
+  async findFavorites(id: string): Promise<Favorites> {
+    const findedUser = await this.find(id);
+
+    if (!findedUser) {
+      throw new Error("User not found");
+    }
+
+    return new Favorites(findedUser.favorites as string);
+  }
+
+  async create(user: User): Promise<User> {
+    this.users.push(user);
+    return user;
+  }
+
   async update(user: User): Promise<User> {
     const findedUser = this.find(user.id);
 
@@ -20,28 +47,5 @@ export class InMemoryUserRepository implements UserRepository {
     });
 
     return findedUser;
-  }
-
-  async find(id: string): Promise<User> {
-    return this.users.filter((user) => user.id === id)[0];
-  }
-
-  async findFavorites(id: string): Promise<Favorites> {
-    const findedUser = await this.find(id);
-
-    if (!findedUser) {
-      throw new Error("User not found");
-    }
-
-    return new Favorites(findedUser.favorites as string);
-  }
-
-  async findMany(): Promise<User[]> {
-    return this.users;
-  }
-
-  async create(user: User): Promise<User> {
-    this.users.push(user);
-    return user;
   }
 }
