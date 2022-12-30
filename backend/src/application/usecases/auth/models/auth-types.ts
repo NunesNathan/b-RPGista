1510e845c4e624d4f2fb4a06e81180c2a6c657dc
Nathan Nunes <nunesnathan@pop-os.localdomain>
2022-12-30 20:39:20 -0300

    feat: jwt models

--- backend/src/application/usecases/auth/models/auth-types.ts
+++ backend/src/application/usecases/auth/models/auth-types.ts
@@ -1,3 +1,6 @@
+import { Request } from "express";
+import { User } from "@application/entities/user";
+
 export interface UserPayload {
   sub: string;
   email: string;
@@ -6,4 +9,14 @@ export interface UserPayload {
 
 export interface JwtToken {
   access_token: string;
+}
+
+export interface UserFromJwtPayload {
+  id: string;
+  email: string;
+  username: string;
+}
+
+export interface AuthRequest extends Request {
+  user: User;
 }
