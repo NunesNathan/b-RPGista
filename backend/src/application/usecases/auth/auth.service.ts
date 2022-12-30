75f8a2616c22730393b92ad5339f0f27df9e5291
Nathan Nunes <nunesnathan@pop-os.localdomain>
2022-12-30 13:01:44 -0300

    feat: local strategy auth

---
+++ backend/src/application/usecases/auth/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class AuthService {
+  login(username: string, password: string) {
+    throw new Error("Method not implemented.");
+  }
+
+  validateUser(email: string, password: string) {
+    throw new Error("Method not implemented.");
+  }
+}
