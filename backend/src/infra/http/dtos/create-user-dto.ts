a3dd8b644bc1105b714012d4d4d89ebd2755c738
Nathan Nunes <nunesnathan@pop-os.localdomain>
2022-12-30 12:23:44 -0300

    feat: block weak passwords

--- backend/src/infra/http/dtos/create-user-dto.ts
+++ backend/src/infra/http/dtos/create-user-dto.ts
@@ -1,4 +1,10 @@
-import { IsEmail, IsNotEmpty, Length } from "class-validator";
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Length,
+  Matches,
+} from "class-validator";
 
 export class CreateUserDto {
   @IsNotEmpty()
@@ -9,6 +15,12 @@ export class CreateUserDto {
   @Length(4, 16)
   username: string;
 
+  @IsString()
   @IsNotEmpty()
+  @Length(4, 16)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: `The password can only contain letters, numbers.
+    Need to contains a lower case letter and one upper case letter.`,
+  })
   password: string;
 }
