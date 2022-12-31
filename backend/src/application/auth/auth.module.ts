c3ac3d00686c7d95cfc186df2bb3e8fecba85d40
Nathan Nunes <nunesnathan@pop-os.localdomain>
2022-12-30 21:03:51 -0300

    feat: check login provided middleware implementation

--- backend/src/application/auth/auth.module.ts
+++ backend/src/application/auth/auth.module.ts
@@ -1,10 +1,11 @@
-import { Module } from "@nestjs/common";
+import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
 import { LocalStrategy } from "@application/usecases/auth/strategies/local.strategy";
 import { DatabaseModule } from "@infra/database/database.module";
 import { AuthController } from "@infra/http/controllers/auth.controller";
 import { AuthService } from "@application/usecases/auth/auth.service";
 import { JwtModule } from "@nestjs/jwt";
 import { JwtStrategy } from "@application/usecases/auth/strategies/jwt.strategy";
+import { LoginValidationMiddleware } from "./middlewares/login-validation.middleware";
 
 @Module({
   imports: [
@@ -19,4 +20,8 @@ import { JwtStrategy } from "@application/usecases/auth/strategies/jwt.strategy"
   controllers: [AuthController],
   providers: [AuthService, LocalStrategy, JwtStrategy],
 })
-export class AuthModule {}
+export class AuthModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoginValidationMiddleware).forRoutes("login");
+  }
+}
