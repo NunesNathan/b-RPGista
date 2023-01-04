b8d2ba2a8f188f2b5f179bc2de169e4edc7ae16b
Nathan Nunes <nunesnathan@pop-os.localdomain>
2023-01-04 13:26:08 -0300

    feat: prerequisite entity

---
+++ backend/src/application/entities/prerequisite/prerequisite.ts
@@ -0,0 +1,34 @@
+import { Replace } from "@helpers/replace";
+import { randomUUID } from "node:crypto";
+
+export enum PrerequisiteType {
+  ATTRIBUTE = "ATTRIBUTE",
+  ANOTHER_SKILL = "ANOTHER_SKILL",
+  ANOTHER_PARANORMAL_POWER = "ANOTHER_PARANORMAL_POWER",
+  OTHER = "OTHER",
+}
+
+interface PrerequisiteProps {
+  prerequisiteId: string;
+  prerequisiteType: PrerequisiteType;
+  prerequisite: string;
+}
+
+export class Prerequisite {
+  private props: PrerequisiteProps;
+
+  constructor(props: Replace<PrerequisiteProps, { prerequisiteId?: string }>) {
+    this.props = {
+      ...props,
+      prerequisiteId: props.prerequisiteId ?? randomUUID(),
+    };
+  }
+
+  public get value(): Prerequisite {
+    return this;
+  }
+
+  public get prerequisiteId(): string {
+    return this.props.prerequisiteId;
+  }
+}
