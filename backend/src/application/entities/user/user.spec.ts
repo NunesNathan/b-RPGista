0bc3ce71c70d1d09dbe5547467424d1efd1ee1ef
NunesNathan <nunesxd7@hotmail.com>
2023-01-06 12:09:19 -0300

    test: user entity

---
+++ backend/src/application/entities/user/user.spec.ts
@@ -0,0 +1,39 @@
+import { Email } from "./email";
+import { User } from "./user";
+import { Username } from "./username";
+
+describe("User entity", () => {
+  const user = new User({
+    username: new Username("test"),
+    email: new Email("test@test.com"),
+    password: "password",
+  });
+
+  it("should be able to create an instance", () => {
+    expect(user).toBeTruthy();
+    expect(user.id);
+    expect(user.email).toEqual("test@test.com");
+    expect(user.username).toEqual("test");
+    expect(user.password).toEqual("password");
+    expect(user.createdAt).toBeTruthy();
+    expect(user.updatedAt).toBeTruthy();
+    expect(user.views).toEqual(0);
+    expect(user.favorites).toEqual('{"count":0,"saved":[]}');
+    expect(user.favoritesCount).toEqual(0);
+    expect(user.favoriteList).toEqual([]);
+  });
+
+  it("should be able to update an instance", () => {
+    user.changeEmail("test2@test.com");
+    expect(user.email).toEqual("test2@test.com");
+
+    user.changePassword("password2");
+    expect(user.password).toEqual("password2");
+  });
+
+  it("should be able to add a view", () => {
+    user.addView();
+    user.addView();
+    expect(user.views).toEqual(2);
+  });
+});
